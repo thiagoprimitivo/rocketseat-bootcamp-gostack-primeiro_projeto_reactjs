17d2c68cc916d741a18feaaeca17444a7a5862f0
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2020-04-18 22:11:59 -0300

    Estilos globais

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,9 +3,5 @@ import React from 'react';
 import { Title } from './styles';
 
 export default function Main() {
-  return (
-    <Title error>
-      Main <small>Menor</small>
-    </Title>
-  );
+  return <Title>Hello!</Title>;
 }
